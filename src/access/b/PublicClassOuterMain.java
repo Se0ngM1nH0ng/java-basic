baaf2d4e41b6b196ba48556bbaf626ea4acb024c
seongminHong1 <137602339+seongminHong1@users.noreply.github.com>
2024-02-14 23:52:06 +0900

    feat: "접근 제어자"

--- src/access/b/PublicClassOuterMain.java
+++ src/access/b/PublicClassOuterMain.java
@@ -8,7 +8,7 @@ public class PublicClassOuterMain {
         PublicClass publicClass = new PublicClass();
 
         //다른 패키지 접근 불가
-//        DefaultClass1 class1 = new DefaultClass1();
+        //DefaultClass1 class1 = new DefaultClass1();
 
     }
 }
