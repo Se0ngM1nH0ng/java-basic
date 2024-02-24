d25c15d17af6e5e0496f63e002f72d4436a08139
seongminHong1 <137602339+seongminHong1@users.noreply.github.com>
2024-02-24 23:29:14 +0900

    Java 메모리구조 와 static 1

---
+++ src/memory/Data.java
@@ -0,0 +1,13 @@
+package memory;
+
+public class Data {
+    private int value;
+
+    public Data(int value) { // alt + insert
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
